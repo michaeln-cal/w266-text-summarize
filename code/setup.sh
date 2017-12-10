37ad3f8664f9cc08f848aa28ae9486023f478d09
Michael Nielsen <michael.nielsen@berkeley.edu>
2017-12-10 00:34:04 -0800

    filter vocab, limit size

--- code/setup.sh
+++ code/setup.sh
@@ -1,15 +1,18 @@
 #!/bin/bash -x
 
-[ ! -f util/create_data.py ] && { echo "** error: run $0 from code directory."; exit 2; }
+# Usage:  ./setup.sh [output_dir]
+#   First arg is output directory, eg: ./setup.sh ../output
+#   All other args are passed straight through to create_data.py
 
-# first arg is output directory, e.g., ../output
-default_args=
-if [ $# -gt 0 ]; then
-    base_dir=$1
-    default_args="--out_dir $base_dir"
-    mkdir "$base_dir"
+[ ! -f util/create_data.py ] &&
+    { echo "** error: run $0 from 'code' directory."; exit 2; }
+
+# if first arg doesn't start with a dash (--foo), assume output dir
+[ $# -gt 0 -a "${1:0:1}" != '-' ] &&
+    default_args="--out_dir $1" &&
     shift
-fi
 
-time python3 util/create_data.py $default_args $@
+# python script takes args in format: --some_arg [some_value]
+# To show usage: python3 util/create_data.py -h
+python3 util/create_data.py $default_args $@
 
--- code/util/create_data.py
+++ code/util/create_data.py
@@ -3,6 +3,8 @@ from __future__ import division
 from __future__ import print_function
 
 import os
+from os.path import join
+import pathlib
 import argparse
 import datetime
 import struct
@@ -66,10 +68,8 @@ def read_abstract_file(filename):
 
     return abstract_text
 
-def write_file(write_dir, filename,data):
-    # if not tf.gfile.Exists(write_dir): tf.gfile.MakeDirs(write_dir)
-
-    with open(write_dir+"/"+filename, "w") as output:
+def write_file(filename, data):
+    with open(filename, "w") as output:
         for s in data:
             output.write(str(s) + '\n')
 
@@ -77,62 +77,116 @@ def write_file(write_dir, filename,data):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Pre-pocess input data.')
 
-    # data_dir ='/Users/giang/Downloads/finished_files/chunked'
-    # target_dir= '../../data/vocab'
-    # vocab_file='../../data/vocab/vocab_original'
+    # default output directories:
+    #   vocab file:  ../output/vocab
+    #   articles:    ../output/articles/*
+    #   abstracts:   ../output/abstract/*
+    #
+    # default input directories:
+    # input: ../data/cnn-dailymail/finished_files/*
+    #    chunked  test.bin  train.bin  val.bin  vocab
+    #
+    # and: ../data/cnn-dailymail/finished_files/chunked/*
+    #    test_*, train_*, val_*
 
-    # for file in os.listdir(data_dir):
-    #     if file.startswith('val_') or file.startswith('train_')or file.startswith('test_') :
-    #         write_file(target_dir+'/article',file,read_article_file(data_dir+'/'+file))
-    #         write_file(target_dir+'/abstract',file,read_abstract_file(data_dir+'/'+file))
+    parser.add_argument('--data_dir', metavar='dir', default='../data/cnn-dailymail/finished_files',
+            help='input dir, default=../data/cnn-dailymail/finished_files/{vocab, chunked}')
 
-    parser.add_argument('--data_dir', metavar='dir',
-            default='../data/cnn-dailymail/finished_files',
-            help='directory for input data (finished_files)')
+    parser.add_argument('--vocab', metavar='file', default=None,
+            help='input original vocab file; default=../data/cnn-dailymail/finished_files/vocab')
 
-    parser.add_argument('--out_dir', metavar='dir',
-            default='../output',
-            help='directory for output data')
+    parser.add_argument('--vocab_special', action='store_true', default=False,
+            help='include numbers and special characters in vocab (default=False)')
+
+    parser.add_argument('--vocab_max_num', metavar='N', type=int, default=150000,
+            help='limit vocab to {N} largest values (for full vocab, use N=0) (default=150000)')
+
+    parser.add_argument('--vocab_min_num', metavar='N', type=int, default=0,
+            help='limit vocab words occuring at least {N} times (for full vocab, use N=0) (default=0)')
+
+    parser.add_argument('--out_dir', metavar='dir', default='../output',
+            help='output dir; default=../output/{vocab, articles/, abstract/}')
 
     args = parser.parse_args()
 
-    print("Running create_data: ", datetime.datetime.now())
+    input_data_dir = join(args.data_dir, 'chunked')
+    input_vocab_file = args.vocab if args.vocab else join(args.data_dir, 'vocab')
 
-    finished_files = args.data_dir
-    data_dir = finished_files + '/chunked'
-    vocab_file= finished_files + '/vocab'
+    target_article_dir = join(args.out_dir, 'article')
+    target_abstract_dir = join(args.out_dir, 'abstract')
+    target_vocab_dir = join(args.out_dir, 'vocab')
 
-    target_dir = args.out_dir
-    target_article_dir= target_dir + '/article'
-    target_abstract_dir= target_dir + '/abstract'
-    target_vocab_dir= target_dir + '/vocab'
-
-    print("Checking inputs/outputs:")
+    print("==Running create_data: ", datetime.datetime.now())
+    print("==Checking outputs:")
     can_continue = True
-    for f in (data_dir, vocab_file, target_dir):
-        found = "[ found ]" if os.path.exists(f) else "[missing]"
-        can_continue = can_continue and os.path.exists(f)
-        print("\t {} \t {}".format(found, f))
+
+    print("==Checking inputs:")
+    for input_file in (input_data_dir, input_vocab_file):
+        found = "[ found ]" if os.path.exists(input_file) else "[missing]"
+        can_continue = can_continue and os.path.exists(input_file)
+        print("\t {} \t {}".format(found, input_file))
 
     if not can_continue:
-        raise ValueError('Unable to find required directories/files. Exiting...')
+        raise ValueError('Input data not found, exiting.')
 
     for subdir in (target_abstract_dir, target_article_dir, target_vocab_dir):
         if os.path.exists(subdir):
-            print("** warning: directory already exists: ", subdir)
+            print("** error: target directory already exists: ", subdir)
+            can_continue = False
         else:
-            print("creating output directory: ", subdir)
-            os.mkdir(subdir)
+            print("...creating output directory: ", subdir)
+            pathlib.Path(subdir).mkdir(parents=True, exist_ok=True)
 
-    print("continuing...")
+    if not can_continue:
+        raise ValueError('Errors found setting up inputs/outputs, exiting.')
 
-    for file in os.listdir(data_dir):
-        if file.startswith('val_') or file.startswith('train_')or file.startswith('test_') :
-            write_file(target_article_dir, file, read_article_file(data_dir+'/'+file))
-            write_file(target_abstract_dir, file, read_abstract_file(data_dir+'/'+file))
+    print("==Continuing...")
 
-    vocab=pd.read_csv(vocab_file,header=None,sep=" ")
-    vocab=vocab.drop_duplicates()
-    vocab=vocab.dropna()
-    vocab = vocab.nlargest(150000,[1])
-    vocab.to_csv(target_dir+"/vocab", columns=[0], index=False, header=False)
+    print("...reading from: %s" % input_data_dir)
+    print("...processing articles to : %s" % target_article_dir)
+    print("...processing abstracts to: %s" % target_abstract_dir)
+
+    #for filename in os.listdir(input_data_dir):
+    #    if filename.startswith('val_') or filename.startswith('train_')or filename.startswith('test_') :
+    #        write_file(join(target_article_dir, filename), read_article_file(join(input_data_dir, filename)))
+    #        write_file(join(target_abstract_dir, filename), read_abstract_file(join(input_data_dir, filename)))
+
+    print("...reading original vocab file (limit: %s): %s" % (args.vocab_max_num, input_vocab_file))
+    df=pd.read_csv(input_vocab_file, header=None, sep=" ")
+    print("...read csv: %s rows (len=%s, count=%s)" % (df.shape[0], len(df.index), df[1].count()))
+
+    #print('...dropping NA:\n%s' % df[df.isnull().any(axis=1)])
+    df.dropna(axis=0, inplace=True)
+    print("...dropped NA: rows=%s" % len(df.index))
+
+    # limit vocab to alpha-numeric words
+    if not args.vocab_special:
+        pattern=r'^[-:\.,0-9]*$'
+        #print('=========dropping:\n%s=========\n' % df[~df[0].str.contains(pattern)])
+        df = df[~df[0].str.contains(pattern)]
+        print("...removed numbers: rows=%s" % len(df.index))
+
+        pattern=r'^[^\w]*$'
+        df = df[~df[0].str.contains(pattern)]
+        print("...removed special: rows=%s" % len(df.index))
+
+    # limit vocab list to words appearing more than N times
+    if args.vocab_min_num:
+        #print("\n====omitting:=====\n%s\n=====\n" % df[df[1] < args.vocab_min_num])
+        df = df[df[1] >= args.vocab_min_num]
+        print("...limit to words occuring at least %s times (rows=%s)" % (args.vocab_min_num, len(df.index)))
+
+    df.drop_duplicates(inplace=True)
+    print("...dedupe: rows=%s" % len(df.index))
+
+    # limit vocab list to the most frequent N words
+    if args.vocab_max_num:
+        df = df.nlargest(args.vocab_max_num, [1])
+        print("...limit to %s rows (verify: rows=%s)" % (args.vocab_max_num, len(df.index)))
+
+    print("...writing new vocab file (rows=%s): %s/vocab" % (len(df.index), target_vocab_dir))
+    df.to_csv(join(target_vocab_dir, 'vocab'), columns=[0], index=False, header=False)
+
+    #print("=========Sample========\n%s" % df)
+    print("==Done setting up data: ", datetime.datetime.now())
+
